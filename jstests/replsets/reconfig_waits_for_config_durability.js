dde40033041b4d7cff041ad73b1691f7649305db
William Schultz <william.schultz@mongodb.com>
2020-02-25 22:29:20 -0500

    Demonstrate durability failure

--- jstests/replsets/reconfig_waits_for_config_durability.js
+++ jstests/replsets/reconfig_waits_for_config_durability.js
@@ -10,7 +10,8 @@ const rst = new ReplSetTest({
         // Turn up the syncdelay (in seconds) to effectively disable background checkpoints.
         syncdelay: 600,
         setParameter: {logComponentVerbosity: tojson({storage: 2})}
-    }
+    },
+    useBridge: true
 });
 rst.startSet();
 rst.initiate();
@@ -19,38 +20,46 @@ assert.commandWorked(rst.getPrimary().getDB("test")["test"].insert({x: 1}));
 rst.awaitReplication();
 rst.awaitLastOpCommitted();
 
-sleep(1000);
+// sleep(1000);
 
 let nodeIdToKill = 1;
 let journalFp = configureFailPoint(rst.nodes[nodeIdToKill], "pauseJournalFlusherThread");
 journalFp.wait();
 
+// let journalFp1 = configureFailPoint(rst.nodes[0], "pauseJournalFlusherThread");
+// journalFp1.wait();
+
 // let checkpointFp = configureFailPoint(rst.nodes[nodeIdToKill], "pauseCheckpointThread");
 // checkpointFp.wait();
 
+// assert.commandWorked(rst.getPrimary().getDB("test")["test"].insert({x: 2}));
+// assert.commandWorked(rst.getPrimary().getDB("test")["test"].insert({x: 2}, {writeConcern:{w:"majority"}}));
 
 // Do a reconfig and wait for propagation.
 jsTestLog("Doing a reconfig.");
 let config = rst.getReplSetConfigFromNode(0);
 
 jsTestLog("Original config: " + tojson(config));
-
 config.version++;
 let newConfigVersion = config.version;
 let start = new Date();
 assert.commandWorked(rst.getPrimary().adminCommand({replSetReconfig: config}));
 rst.awaitNodesAgreeOnConfigVersion();
-sleep(500);
 jsTestLog("Finished waiting for reconfig to propagate. Took: " + (new Date() - start) + "ms");
+
+rst.nodes[1].disconnect([rst.nodes[0], rst.nodes[2]]);
 
 jsTestLog("Kill and restart a secondary node.");
 rst.stop(nodeIdToKill, 9, {allowedExitCode: MongoRunner.EXIT_SIGKILL}, {forRestart: true});
 rst.start(nodeIdToKill, undefined, true /* restart */);
 
-rst.awaitNodesAgreeOnConfigVersion();
 config = rst.getReplSetConfigFromNode(nodeIdToKill);
 jsTestLog("New config: " + tojson(config));
 assert.eq(config.version, newConfigVersion);
+
+rst.nodes[1].reconnect([rst.nodes[0], rst.nodes[2]]);
+
+rst.awaitNodesAgreeOnConfigVersion();
 
 rst.stopSet();
 }());
