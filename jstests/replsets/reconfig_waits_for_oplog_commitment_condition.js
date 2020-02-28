ff41cf47a07d0c1874033d8bbff6feb372359c34
William Schultz <william.schultz@mongodb.com>
2020-02-28 17:46:57 -0500

    Add some notes

--- jstests/replsets/reconfig_waits_for_oplog_commitment_condition.js
+++ jstests/replsets/reconfig_waits_for_oplog_commitment_condition.js
@@ -44,61 +44,85 @@ jsTestLog("Test that reconfig waits for last op committed in previous config.");
 // which replication is currently paused. The reconfig should initially fail due to a timeout.
 // Retrying the reconfig with a higher version should also fail since the op won't be committed in
 // the current config.
-function testSafeReconfigRemove() {
-    let config = rst.getReplSetConfigFromNode();
-    let count = config.members.length - 1;
 
-    jsTestLog("Reconfiguring down to " + count + " nodes.");
-    // Stop replication to a maximal minority of nodes in the current config.
-    let currMajority = Math.floor(config.members.length / 2) + 1;
-    let minority = rst.nodes.slice(0, config.members.length).slice(currMajority);
-    stopServerReplication(minority);
-    jsTestLog("Stopped server replication on: " + tojson(minority));
+// Test a node removal case. Commit an op on 3/5 nodes. If you remove one of the nodes
+// from the write majority from the config, there will now be 2/4 nodes in the new config with the
+// op, which means the op is not committed in that config.
 
-    // Commit an op in this config with the minimum set of nodes needed to satisfy a majority.
-    assert.commandWorked(coll.insert({x: count}, {writeConcern: {w: "majority"}}));
+// Test a node addition case. Commit an op on 2/3 nodes. If you add one more node to the set this
+// means that the original op is no longer committed in the new config.
 
-    // Remove one node from the config.
-    let newConfigMembers = config.members.slice(0, count);
-    let newConfigNodes = config.members.slice(0, count);
-    let origMembers = config.members;
-    config.members = newConfigMembers;
-    config.version++;
-
-    // Commit an op in this config with the minimum set of nodes needed to satisfy a majority.
-    assert.commandWorked(coll.insert({x: count}, {writeConcern: {w: "majority"}}));
-
-    // Attempt to move to new config. Expect failure since the op cannot commit in the new config.
-    assert.commandFailedWithCode(primary.adminCommand({replSetReconfig: config, maxTimeMS: 1000}),
-                                 ErrorCodes.MaxTimeMSExpired);
-
-    // Leaving the current config should be prohibited since the op is not committed.
-    config.version++
-    assert.commandFailedWithCode(primary.adminCommand({replSetReconfig: config, maxTimeMS: 1000}),
-                                 ErrorCodes.ConfigurationInProgress);
-
-    // Restart replication to one node of the minority. Reconfig should now eventually succeed.
-    restartServerReplication(minority[0]);
-    assert.soonNoExcept(function() {
-        assert.commandWorked(primary.adminCommand({replSetReconfig: config}));
-        return true;
-    });
-
-    // Restart replication to all minority members.
-    assert.soonNoExcept(function() {
-        restartServerReplication(minority);
-        return true;
-    });
-}
+// In general, goal is to test cases where an op that can be committed in W nodes in config Ci
+// would not be committed with the same number of nodes in Cj.
+//
+// Node add cases:
+// 1 (w:1) -> 2 (w:2)
+// 2 (w:2) -> 3 (w:2)
+// 3 (w:2) -> 4 (w:3)
+// 4 (w:3) -> 5 (w:3)
+//
+// Node remove cases:
+// 5 (w:3) -> 4 (w:3)
+// 4 (w:3) -> 3 (w:2)
+// 3 (w:2) -> 2 (w:2)
+// 2 (w:2) -> 1 (w:1)
+//
+// For adds, if you move from an ODD -> EVEN config, then safety is not guaranteed.
+// For removes, if you move from EVEN -> ODD config, then safety is not guaranteed.
 
 let origConfig = rst.getReplSetConfigFromNode();
 
+let config = rst.getReplSetConfigFromNode();
+let count = config.members.length - 1;
+
+jsTestLog("Reconfiguring down to " + count + " nodes.");
+// Stop replication to a maximal minority of nodes in the current config.
+let currMajority = Math.floor(config.members.length / 2) + 1;
+let minority = rst.nodes.slice(0, config.members.length).slice(currMajority);
+stopServerReplication(minority);
+jsTestLog("Stopped server replication on: " + tojson(minority));
+
+// Commit an op in this config with the minimum set of nodes needed to satisfy a majority.
+assert.commandWorked(coll.insert({x: count}, {writeConcern: {w: "majority"}}));
+
+// Remove one node from the config.
+let newConfigMembers = config.members.slice(0, count);
+let newConfigNodes = config.members.slice(0, count);
+let origMembers = config.members;
+config.members = newConfigMembers;
+config.version++;
+
+// Commit an op in this config with the minimum set of nodes needed to satisfy a majority.
+assert.commandWorked(coll.insert({x: count}, {writeConcern: {w: "majority"}}));
+
+// Attempt to move to new config. Expect failure since the op cannot commit in the new config.
+assert.commandFailedWithCode(primary.adminCommand({replSetReconfig: config, maxTimeMS: 1000}),
+                             ErrorCodes.MaxTimeMSExpired);
+
+// Leaving the current config should be prohibited since the op is not committed.
+config.version++
+assert.commandFailedWithCode(primary.adminCommand({replSetReconfig: config, maxTimeMS: 1000}),
+                             ErrorCodes.ConfigurationInProgress);
+
+// Restart replication to one node of the minority. Reconfig should now eventually succeed.
+restartServerReplication(minority[0]);
+assert.soonNoExcept(function() {
+    assert.commandWorked(primary.adminCommand({replSetReconfig: config}));
+    return true;
+});
+
+// Restart replication to all minority members.
+assert.soonNoExcept(function() {
+    restartServerReplication(minority);
+    return true;
+});
+
 // Remove 1 node at a time and test safe reconfig succeeds in correct scenario.
-assert.eq(rst.getReplSetConfigFromNode().members.length, 5);
-testSafeReconfigRemove();
+// assert.eq(rst.getReplSetConfigFromNode().members.length, 5);
+// testSafeReconfigRemove();
 assert.eq(rst.getReplSetConfigFromNode().members.length, 4);
-testSafeReconfigRemove();
-assert.eq(rst.getReplSetConfigFromNode().members.length, 3);
+// testSafeReconfigRemove();
+// assert.eq(rst.getReplSetConfigFromNode().members.length, 3);
 
 // Force reconfig back to the original set to stop test.
 origConfig.version = rst.getReplSetConfigFromNode().version + 1;
