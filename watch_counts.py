92cf5228f9ed63ee35f4009460bc5855c545b440
William Schultz <william.schultz@mongodb.com>
2020-04-20 23:30:59 -0400

    Add watchpoint script

---
+++ watch_counts.py
@@ -0,0 +1,49 @@
+import gdb
+
+#
+# Trace sizeStorer updates via hardware watchpoints.
+#
+
+# Go to the start of the program.
+gdb.execute("ugo start")
+
+# Delete all breakpoints.
+gdb.execute("delete")
+
+# Turn on logging to external gdb.txt file.
+gdb.execute("set print thread-events off")
+
+# Don't print structures in call frame args.
+gdb.execute("set print frame-arguments none")
+
+gdb.execute("set pagination off")
+gdb.execute("set logging overwrite on")
+gdb.execute("set logging off")
+gdb.execute("set logging on")
+
+# The collection whose counts we want to inspect.
+nss = "test.coll1"
+
+# Set breakpoint on when we increment the number of records on a collection (record store).
+class SizeStorerBreakpoint(gdb.Breakpoint):
+    # Commands to execute when the breakpoint is hit.
+    def stop (self):
+        if bool(gdb.parse_and_eval('$_streq(_ns.c_str(), "%s")' % nss)):
+            # Set a hardware watchpoint for future updates.
+            gdb.execute('set $addr1 = &(_sizeInfo->numRecords._value._M_i)')
+            gdb.execute('watch *$addr1')
+            gdb.execute("bt 8")
+            gdb.execute("uinfo time")
+            print("")
+            return True
+        # Don't continue automatically.
+        return False
+
+# Enable the breakpoint.       
+SizeStorerBreakpoint("_changeNumRecords", temporary=True)
+
+# Run the recorded execution until we hit the breakpoint which will set the watchpoint.
+gdb.execute("continue")
+gdb.execute("bt 8")
+
+# At this point we can manually run 'continue' to inspect each location at which we hit the watchpoint.
