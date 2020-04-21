ba48bcd7be7b53842621be40d01bb1b30de24c87
William Schultz <william.schultz@mongodb.com>
2020-04-20 22:10:34 -0400

    Add some obfuscation to test for demo purposes

--- prepare_txn_counts.js
+++ prepare_txn_counts.js
@@ -1,51 +1,92 @@
 
-load("jstests/core/txns/libs/prepare_helpers.js");
 load("jstests/hooks/validate_collections.js");
+load("jstests/core/txns/libs/prepare_helpers.js");
 
-const name = "prepare_counts";
+//
+// Simplified version of the original test discovered by the rollback fuzzer in BF-15552. Tries to 
+// simulate some of the randomness in the original generated test for sake of debugging demonstration.
+//
+
+const name = "bf-15552-demo";
 const rst = new ReplSetTest({nodes: 1});
 const nodes = rst.startSet();
 rst.initiate();
 const dbName = "test";
-const collName = name;
+const collName1 = "coll1";
+const collName2 = "coll2";
 
-const primary = rst.getPrimary();
-const secondary = rst.getSecondary();
-const testDB = primary.getDB(dbName);
+let primary = rst.getPrimary();
+let secondary = rst.getSecondary();
+let testDB = primary.getDB(dbName);
 
-assert.commandWorked(testDB.runCommand({create: collName, writeConcern: {w: "majority"}}));
+// Create a few collections.
+assert.commandWorked(testDB.runCommand({create: collName1, writeConcern: {w: "majority"}}));
+assert.commandWorked(testDB.runCommand({create: collName2, writeConcern: {w: "majority"}}));
 
-const session = primary.startSession({causalConsistency: false});
-const sessionDB = session.getDatabase(dbName);
-const sessionColl = sessionDB.getCollection(collName);
+// Create two sessions.
+const session1 = primary.startSession({causalConsistency: false});
+const session1DB = session1.getDatabase(dbName);
+const session1Coll = session1DB.getCollection(collName1);
 
-jsTestLog("Inserting 4 documents");
-for(var i=0;i<4;i++){
-    assert.commandWorked(primary.getDB(dbName).getCollection(collName).insert({x:i}));
-}
+const session2 = primary.startSession({causalConsistency: false});
+const session2DB = session2.getDatabase(dbName);
+const session2Coll = session2DB.getCollection(collName2);
 
-jsTestLog("Starting transaction.");
-session.startTransaction();
-var txnNum = session.getTxnNumber_forTesting();
-assert.commandWorked(sessionColl.insert({_id:1}));
-assert.commandWorked(sessionColl.insert({_id:2}));
-jsTestLog("Preparing transaction.");
-PrepareHelpers.prepareTransaction(session);
+//
+// Do some random operations on both collections.
+//
+assert.commandWorked(primary.getDB(dbName).getCollection(collName1).insert({x:1}));
+assert.commandWorked(primary.getDB(dbName).getCollection(collName2).insert({x:2}));
+assert.commandWorked(primary.getDB(dbName).getCollection(collName2).insert({x:3}));
+assert.commandWorked(primary.getDB(dbName).getCollection(collName2).remove({x:2}));
+assert.commandWorked(primary.getDB(dbName).getCollection(collName1).insert({x:4}));
+assert.commandWorked(primary.getDB(dbName).getCollection(collName1).update({x:4}, {$set: {x:6}}));
+assert.commandWorked(primary.getDB(dbName).getCollection(collName1).insert({x:3}));
+assert.commandWorked(primary.getDB(dbName).getCollection(collName1).insert({x:4}));
 
+//
+// Do some transaction operations.
+//
+session2.startTransaction();
+var txnNum = session2.getTxnNumber_forTesting();
+assert.commandWorked(session2Coll.insert({_id:1}));
+assert.commandWorked(session2Coll.insert({_id:2}));
+session2.commitTransaction();
 
+session1.startTransaction();
+var txnNum = session1.getTxnNumber_forTesting();
+assert.commandWorked(session1Coll.insert({_id:3}));
+assert.commandWorked(session1Coll.insert({_id:4}));
+PrepareHelpers.prepareTransaction(session1);
+
+// Do some more operations.
+assert.commandWorked(primary.getDB(dbName).getCollection(collName1).insert({x:4}));
+assert.commandWorked(primary.getDB(dbName).getCollection(collName1).update({x:4}, {$set: {x:6}}));
+assert.commandWorked(primary.getDB(dbName).getCollection(collName1).insert({x:3}));
+assert.commandWorked(primary.getDB(dbName).getCollection(collName1).insert({x:4}));
+
+// Do some more transaction operations.
+session2.startTransaction();
+var txnNum = session2.getTxnNumber_forTesting();
+assert.commandWorked(session2Coll.insert({_id:4}));
+assert.commandWorked(session2Coll.insert({_id:5}));
+assert.commandWorked(session2Coll.insert({_id:6}));
+assert.commandWorked(session2Coll.insert({_id:7}));
+session2.abortTransaction();
+
+// Restart the primary node.
 jsTestLog("Restarting node.");
 rst.restart(0);
 
-// jsTestLog("Validating collection.");
-// assert.commandWorked(primary.adminCommand({replSetStepDown: 5, force: true}));
-// assert.commandWorked(testDB.runCommand({validate: collName, full: true}));
-
-var session1 = PrepareHelpers.createSessionWithGivenId(rst.getPrimary(), session.getSessionId());
-var session1DB = session1.getDatabase(dbName);
-
-jsTestLog("Aborting transaction.");
-assert.commandWorked(session1DB.adminCommand(
-    {abortTransaction: 1, txnNumber: txnNum, stmtid: NumberInt(3), autocommit: false}));
+// Abort all remaining open transactions before stopping the set.
+primary = rst.getPrimary();
+let res = primary.adminCommand({currentOp: 1, "transaction": {$exists: true}});
+jsTestLog(res);
+res.inprog.forEach(op => {
+    let sess = PrepareHelpers.createSessionWithGivenId(rst.getPrimary(), op.lsid);
+    let sessDB = sess.getDatabase(dbName);
+    assert.commandWorked(sessDB.adminCommand({abortTransaction: 1, txnNumber: op.transaction.parameters.txnNumber, stmtid: NumberInt(3), autocommit: false}));
+})
 
 rst.checkCollectionCounts();
 rst.stopSet();
